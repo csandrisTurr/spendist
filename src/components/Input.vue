963fc74a97d3a4886243c3ea333426d9a69e4ea8
csandrisTurr <csaszarandras@turr.hu>
2023-12-04 11:00:35 +0100

    ref: remove unnecessary field

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -5,6 +5,6 @@ const textModel = defineModel();
 
 <template>
     <input :type="type" :name="name" :id="id" v-model="textModel" :placeholder="placeholder"
-        class="p-2 rounded-sm bg-zinc-900 text-zinc-400 outline-emerald-500 selection:bg-emerald-400 selection:text-zinc-900 shadow-none border-none"
+        class="p-2 rounded-sm bg-zinc-900 text-zinc-400 selection:bg-emerald-400 selection:text-zinc-900 shadow-none border-none"
     >
 </template>
