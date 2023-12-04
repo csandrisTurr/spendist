a9c9f5bdd6ee47fa72e01ce14f42a2e0f88f1ed4
csandrisTurr <csaszarandras@turr.hu>
2023-12-04 11:23:40 +0100

    feat: logout

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,6 +7,7 @@ import { useUserStore } from '@/stores/user';
 import router from '@/router';
 
 const userStore = useUserStore();
+const classes = "flex items-center gap-2 px-4 py-2 rounded-md text-zinc-300 ease-in-out hover:bg-zinc-900 duration-300 text-lg";
 
 function capitalize(str: string): string {
     return `${str[0].toUpperCase()}${str.slice(1)}`;
@@ -16,22 +17,35 @@ const routes = computed(() => router.getRoutes().filter(x => {
     return (x.meta['public'] && !userStore.loggedIn()) ||
            (!x.meta['public'] && userStore.loggedIn());
 }));
+
+function logout() {
+    userStore.setUser(null);
+    router.push('login');
+}
 </script>
 
 <template>
     <nav class="flex flex-col border-r-zinc-800 border-r-[1px] p-4 gap-4 min-w-[200px]">
         <span class="text-emerald-500 font-mono font-bold text-3xl w-full text-center title">spendist</span>
         <hr class="border-zinc-700 w-5/6 self-center">
-        <div class="flex flex-col gap-2">
+        <div class="flex flex-col gap-2 h-full">
             <RouterLink
                 v-for="route in routes"
                 :key="route.name"
                 :to="route.path"
-                class="flex items-center gap-2 px-4 py-2 rounded-md text-zinc-300 ease-in-out hover:bg-zinc-900 duration-300 text-lg"
+                :class="classes"
             >
                 <Icon>{{ route.meta['iconOverride'] || route.name }}</Icon>
                 <span>{{ capitalize(route.name as string) }}</span>
             </RouterLink>
+            <span 
+                v-if="userStore.loggedIn()"
+                :class="classes"
+                class="text-red-400 hover:bg-red-400/10 cursor-pointer justify-self-end mt-auto"
+            >
+                <Icon>logout</Icon>
+                <span @click="logout">Log out</span>
+            </span>
         </div>
     </nav>
 </template>
