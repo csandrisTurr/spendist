4bbd4d7f1aa93bb33d76244b933553e017aea7a4
csandrisTurr <csaszarandras@turr.hu>
2023-12-06 09:15:22 +0100

    feat: add axios

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios';
 
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueAxios, axios)
 
 app.mount('#app')
