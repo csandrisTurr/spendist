69a896d26bd979f65e69fdd81b600dec3e4bb5ad
csandrisTurr <csaszarandras@turr.hu>
2023-12-06 09:41:14 +0100

    feat: pressedEnter input event

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
 defineProps(['type', 'name', 'id', 'placeholder']);
 const textModel = defineModel();
+const emit = defineEmits(['pressedEnter']);
+
+function handleKeypress(e: KeyboardEvent) {
+  if (e.key == 'Enter') {
+    emit('pressedEnter');
+  }
+}
 </script>
 
 <template>
-    <input :type="type" :name="name" :id="id" v-model="textModel" :placeholder="placeholder"
+    <input @keypress="handleKeypress" :type="type" :name="name" :id="id" v-model="textModel" :placeholder="placeholder"
         class="p-2 rounded-sm bg-zinc-900 text-zinc-400 selection:bg-emerald-400 selection:text-zinc-900 shadow-none border-none outline-none"
     >
 </template>
