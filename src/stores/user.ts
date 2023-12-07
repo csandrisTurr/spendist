78f04f2d0256be3034e7c5947cdb82e5f139a389
csandrisTurr <csaszarandras@turr.hu>
2023-12-07 13:14:40 +0100

    feat: method to get logged in user

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -12,5 +12,9 @@ export const useUserStore = defineStore('user', () => {
     return currentUser.value != null;
   }
 
-  return { setUser, loggedIn };
+  function getUser(): any {
+    return currentUser.value;
+  }
+
+  return { setUser, loggedIn, getUser };
 })
