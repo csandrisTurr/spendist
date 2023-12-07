c087e2127713e29d40926b12098b83bada7b7854
csandrisTurr <csaszarandras@turr.hu>
2023-12-07 13:01:56 +0100

    feat: cards in home view

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps(['title', 'value']);
+</script>
+
+<template>
+    <div class="flex flex-col bg-zinc-900 p-6 rounded-md shadow-lg">
+        <span class="text-lg">{{ title }}</span>
+        <span class="font-bold text-4xl">{{ value }}</span>
+    </div>
+</template>
