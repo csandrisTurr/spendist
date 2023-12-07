9dbe3220798564fed1ab61d83663aac57bef1df7
csandrisTurr <csaszarandras@turr.hu>
2023-12-07 13:04:05 +0100

    ref: rename homecard to valuecard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import HomeCard from '@/components/HomeCard.vue';
+import ValueCard from '@/components/ValueCard.vue';
 </script>
 
 <template>
   <main class="w-full">
     <section class="grid gap-4 grid-rows-1 xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2">
-      <HomeCard title="Osszes bevetel" value="304 683 Ft" />
-      <HomeCard title="Osszes kiadas" value="95 332 Ft" />
-      <HomeCard title="30 napos bevetel" value="20 000 Ft" />
-      <HomeCard title="30 napos kiadas" value="30 215 Ft" />
+      <ValueCard title="Osszes bevetel" value="304 683 Ft" />
+      <ValueCard title="Osszes kiadas" value="95 332 Ft" />
+      <ValueCard title="30 napos bevetel" value="20 000 Ft" />
+      <ValueCard title="30 napos kiadas" value="30 215 Ft" />
     </section>
   </main>
 </template>
