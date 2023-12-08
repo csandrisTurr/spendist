8866d129a1ed27475279051350cce43cb20bcc47
csandrisTurr <csaszarandras@turr.hu>
2023-12-08 09:34:55 +0100

    feat: made chart look nice

--- src/views/AnalyticsView.vue
+++ src/views/AnalyticsView.vue
@@ -6,47 +6,47 @@ import { LineChart } from 'echarts/charts'
 import { GridComponent } from 'echarts/components'
 import { CanvasRenderer } from 'echarts/renderers'
 import type { EChartsOption } from 'echarts';
+import colors from 'tailwindcss/colors';
+import { install$10 } from 'echarts/types/dist/shared';
 
 use([GridComponent, LineChart, CanvasRenderer]);
 
 let option: EChartsOption = {
-  color: '#FF0',
+  color: colors['emerald']['800'],
   textStyle: {
-    color: '#000',
+    color: colors['zinc']['500'],
   },
   xAxis: {
     type: 'category',
     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    nameTextStyle: {
-      color: '#F00',
-    },
-    splitLine: {
-      lineStyle: {
-        color: '#F00',
-      },
-    },
     axisLine: {
       lineStyle: {
-        color: '#FF0',
-      }
+        color: colors['zinc']['500'],
+        width: 2,
+      },
     }
   },
   yAxis: {
     type: 'value',
+    splitLine: {
+      lineStyle: {
+        color: colors['zinc']['800'],
+        width: 2,
+      }
+    }
   },
   series: [
     {
       data: [820, 932, 901, 934, 1290, 1330, 1320],
       type: 'line',
-      smooth: true,
+      smooth: false,
       lineStyle: {
-        color: '#34d399',
-        width: 3,
+        color: colors['emerald']['500'],
+        width: 5,
+        shadowColor: colors['emerald']['500'],
+        shadowBlur: 15,
+        shadowOffsetY: 10,
       },
-      areaStyle: {
-        opacity: 0,
-        shadowColor: '#34d399',
-      }
     }
   ],
 };
@@ -60,7 +60,7 @@ let option: EChartsOption = {
       <ValueCard title="30 day earnings" value="20 000 Ft" />
       <ValueCard title="30 day spendings" value="30 215 Ft" />
     </section>
-    <section class="bg-zinc-900 h-full">
+    <section class="bg-zinc-900 h-full rounded-md flex items-center justify-center">
       <v-chart :option="option" autoresize class="chart"></v-chart>
     </section>
   </main>
@@ -68,6 +68,6 @@ let option: EChartsOption = {
 
 <style>
 .chart {
-  height: 700px;
+  height: 600px;
 }
 </style>
