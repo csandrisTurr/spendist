3fee892376fb51a299172505c8e97d3b68888c96
csandrisTurr <csaszarandras@turr.hu>
2023-12-11 15:55:02 +0100

    feat: button for adding data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Sidebar from './components/Sidebar.vue';
+import Icon from './components/Icon.vue';
 
 function isAuthenticated(): boolean {
   
@@ -11,6 +12,10 @@ function isAuthenticated(): boolean {
   <Sidebar />
 
   <RouterView class="p-4" />
+
+  <div class="absolute right-8 bottom-8 h-16 w-16 bg-emerald-500 rounded-full flex justify-center items-center shadow">
+    <Icon class="text-3xl">add</Icon>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/views/NewDataView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>newdata</main>
+</template>
+
+<style lang="scss">
+</style>
