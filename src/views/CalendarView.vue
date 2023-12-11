6fea6818abfb3e64dc16b5671315a5e5ea7f6fe0
csandrisTurr <csaszarandras@turr.hu>
2023-12-11 14:06:03 +0100

    feat: styled fullcalendar a bit

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -5,12 +5,30 @@ import FullCalendar from '@fullcalendar/vue3';
 
 const calendarOptions: CalendarOptions = {
   initialView: 'dayGridMonth',
-  plugins: [dayGridPlugin]
+  plugins: [dayGridPlugin],
 }
 </script>
 
 <template>
-  <main>
-    <FullCalendar :options="calendarOptions"></FullCalendar>
+  <main class="w-full">
+    <FullCalendar :options="calendarOptions" class="w-full h-full fc"></FullCalendar>
   </main>
 </template>
+
+<style lang="scss">
+.fc-toolbar-title {
+  @apply text-3xl font-bold #{!important};
+}
+
+.fc-button {
+  @apply bg-emerald-500 #{!important};
+}
+
+.fc-day-today {
+  @apply bg-emerald-500/50 #{!important};
+}
+
+.fc * {
+  @apply border-zinc-600 #{!important};
+}
+</style>
