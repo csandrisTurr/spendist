0572fff59b54ac18d9887d99fc2399ab42d32e99
csandrisTurr <csaszarandras@turr.hu>
2023-12-11 13:17:17 +0100

    feat: initialized fullcalendar

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import type { CalendarOptions } from '@fullcalendar/core/index.js';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/vue3';
+
+const calendarOptions: CalendarOptions = {
+  initialView: 'dayGridMonth',
+  plugins: [dayGridPlugin]
+}
 </script>
 
 <template>
   <main>
-    calendar
+    <FullCalendar :options="calendarOptions"></FullCalendar>
   </main>
 </template>
