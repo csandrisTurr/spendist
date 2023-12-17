07da22863eb469ca82c43e32799450b9211193e8
csandrisTurr <csaszarandras@turr.hu>
2023-12-17 13:27:55 +0100

    feat: button types

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+    'type': {
+        default: 'primary',
+        type: String,
+    }
+});
+
+const buttonTypeMap = {
+    'primary': 'bg-emerald-500 hover:bg-emerald-400 active:bg-emerald-600',
+    'secondary': 'bg-zinc-500 hover:bg-zinc-400 active:bg-zinc-600',
+    'warning': 'bg-red-500 hover:bg-red-400 active:bg-red-600',
+};
+</script>
+
 <template>
-    <button class="bg-emerald-500 font-bold rounded-sm p-2 hover:bg-emerald-400 ease-out duration-200 active:bg-emerald-600"><slot></slot></button>
+    <button
+        class="font-bold rounded-sm p-2 ease-out duration-200 flex items-center justify-center"
+        :class="buttonTypeMap[type as keyof typeof buttonTypeMap]"
+        ><slot></slot></button>
 </template>
